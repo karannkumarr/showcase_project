84ac09364ae6d3250546541ec6fe170cbdd66c1c
karannkumar <karannkumarrao.08@gmail.com>
2024-05-02 23:08:12 +0530

    first

---
+++ common.types.ts
@@ -0,0 +1,66 @@
+import { User, Session } from "next-auth";
+
+export type FormState = {
+	title: string;
+	description: string;
+	image: string;
+	liveSiteUrl: string;
+	githubUrl: string;
+	category: string;
+};
+
+export interface ProjectInterface {
+	title: string;
+	description: string;
+	image: string;
+	liveSiteUrl: string;
+	githubUrl: string;
+	category: string;
+	id: string;
+	createdBy: {
+		name: string;
+		email: string;
+		avatarUrl: string;
+		id: string;
+	};
+}
+
+export interface UserProfile {
+	id: string;
+	name: string;
+	email: string;
+	college: string;
+	course: string;
+	year: string;
+	description: string | null;
+	avatarUrl: string;
+	githubUrl: string | null;
+	linkedinUrl: string | null;
+	projects: {
+		edges: { node: ProjectInterface }[];
+		pageInfo: {
+			hasPreviousPage: boolean;
+			hasNextPage: boolean;
+			startCursor: string;
+			endCursor: string;
+		};
+	};
+}
+
+export interface SessionInterface extends Session {
+	user: User & {
+		id: string;
+		name: string;
+		email: string;
+		avatarUrl: string;
+	};
+}
+
+export interface ProjectForm {
+	title: string;
+	description: string;
+	image: string;
+	liveSiteUrl: string;
+	githubUrl: string;
+	category: string;
+}
