ab60d881ed7808cb747639a52cc9fddd837572c5
karannkumar <karannkumarrao.08@gmail.com>
2024-05-06 14:17:02 +0530

    see

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-// Home.tsx
-"use client"
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect, Suspense } from 'react';
 import ProjectCard from '@/components/ProjectCard';
-import imageone from "../app/assets/imageone.jpg";
 import Categories from '@/components/Categories';
 import { useSearchParams } from 'next/navigation';
 
@@ -25,55 +22,53 @@ interface Project {
   createdBy: string; // Assuming createdBy is the ID of the user who created the project
 }
 
-
-
 const Home = () => {
-
-  var searchParams = useSearchParams()
-  var categories = searchParams.get("category") || null
-
-  const [projects, setProjects] = useState<Project[]>([]); // Specify the type of projects as Project[]
+  const [projects, setProjects] = useState<Project[]>([]);
+  const searchParams = useSearchParams();
+  const categories = searchParams.get("category") || null;
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch(`/api/user?type=project&category=${categories}`); // Send request with query parameter
+        const response = await fetch(`/api/user?type=project&category=${categories}`);
         if (!response.ok) {
           throw new Error('Failed to fetch projects');
         }
         const data = await response.json();
-        console.log('Response data:', data); // Log the response data
+        console.log('Response data:', data);
         setProjects(data.projects);
       } catch (error) {
         console.error('Error fetching projects:', error);
       }
     };
-  
+
     fetchProjects();
   }, [categories]);
-  
 
   return (
     <section className="flexStart flex-col paddings mb-16">
-      <Categories/>
-   
-  <section className="projects-grid">
-  {projects.map(project => (
-  <ProjectCard
-    key={project._id} // Use _id as the key
-    id={project._id} // Use _id as the id
-    image={ project.image }
-    title={`${project.title}`} // Use _id in the title
-    name={"Karan"}
-    avatarUrl={project.image}
-    userId={project.createdBy|| ''} // Use an appropriate fallback if createdBy or id is missing or undefined
-  />
-))}
-
+      <Categories />
+      <section className="projects-grid">
+        {projects.map(project => (
+          <ProjectCard
+            key={project._id}
+            id={project._id}
+            image={project.image}
+            title={`${project.title}`}
+            name={"Karan"}
+            avatarUrl={project.image}
+            userId={project.createdBy || ''}
+          />
+        ))}
       </section>
-      
     </section>
   );
 };
 
-export default Home;
+const SuspendedHome = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Home />
+  </Suspense>
+);
+
+export default SuspendedHome;
